3d43fabea494464477ab2cc2fb48053a1d911fbf
pradnyagandhle96 <pradnyagandhale25>
2024-07-31 13:04:38 -0600

    add commment

---
+++ Javascript/operator.js
@@ -0,0 +1,81 @@
+console.log();
+
+// let a = 10;
+// let b = 20;
+// let res = a+b;
+// b++;
+
+// console.log(`${a} + ${b} =${res}`);
+
+// function add(a,b)
+// {
+//     return a*b;
+
+// }
+// console.log(add(a,b));
+
+// let x=10;
+// let y = 10;
+// console.log(x==y);  //==compare values ===compare datatype //eg."10"    and 10  compare string and int
+
+// let x = 10;
+// let y = "10";
+// console.log(x!==y);
+
+// let x1 = 10;
+// let y2 = 20;
+
+// console.log(x1>=y2);
+
+// let x3 = 10;
+// let y4 = 20;
+
+// console.log(x3<y4);
+
+// let x5 = 10;
+// let y6 = 20;
+
+// console.log(x5<y6);
+
+// let c = function(a,b)
+// {
+//     return a + b;
+
+// }
+// console.log(c(10,30));
+
+// P=10;
+// Q = 50;
+
+// if(P>10 && Q>10)
+// {
+// console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
+
+// j=49;
+// k=45;
+
+// if(P>10 || Q>10)
+//     {
+//     console.log(true);
+//     }
+//     else {
+//         console.log(false);
+//     } 
+
+    g=57;
+    e=56;
+    if(g>10 != e>10)
+        {
+        console.log(true);
+        }
+        else {
+            console.log(false);
+        }
+
+
+
+                           
